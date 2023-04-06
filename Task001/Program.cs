5c173a25bd35253d073e6a3784a7ca192b472fe8
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-04-06 18:55:14 +0300

    add Task001 Home work 8

---
+++ Task001/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = FillArray(rows, cols);
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+Console.WriteLine("Сортировка по строкам: ");
+
+int[] row = new int[rows];
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+        row[j] = array[i, j];
+    BubbleSort(row);
+    Insert(true, i, row, array);
+}
+PrintArray(array);
+
+
+
+
+void Insert(bool isRow, int dim, int[] source, int[,] dest)
+{
+    for (int k = 0; k < source.Length; k++)
+    {
+        if (isRow)
+            dest[dim, k] = source[k];
+        else
+            dest[k, dim] = source[k];
+    }
+}
+
+int[,] FillArray(int t, int i)
+{
+    int[,] table = new int[t, i];
+    Random random = new Random();
+    for (int a = 0; a < t; a++)
+    {
+        for (int b = 0; b < i; b++)
+        {
+            table[a, b] = random.Next(0, 9);
+        }
+    }
+    return table;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+        for (int b = 0; b < array.GetLength(1); b++)
+        {
+            Console.Write(array[a, b] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void BubbleSort(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+        for (int j = 0; j < inArray.Length - i - 1; j++)
+        {
+            if (inArray[j] > inArray[j + 1])
+            {
+                int temp = inArray[j];
+                inArray[j] = inArray[j + 1];
+                inArray[j + 1] = temp;
+            }
+        }
+}
+
+
+
